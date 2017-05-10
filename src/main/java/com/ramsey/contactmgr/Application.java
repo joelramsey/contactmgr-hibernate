292d1926bff13c286bc78401164c4688990fd354
joelramsey <joel.d.ramsey@gmail.com>
2017-05-09 21:47:51 -0600

    added session factory

--- src/main/java/com/ramsey/contactmgr/Application.java
+++ src/main/java/com/ramsey/contactmgr/Application.java
@@ -1,7 +1,24 @@
 package com.ramsey.contactmgr;
 
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import javax.imageio.spi.ServiceRegistry;
+
 public class Application {
+
+    //Reusable ref to a session factory
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        //Creating a standard service registry object
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
     public static void main(String[] args) {
 
     }
