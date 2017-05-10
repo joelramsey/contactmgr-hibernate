3062ce2d1824ab8f94a0b7b8d6a5915332d90b6f
joelramsey <joel.d.ramsey@gmail.com>
2017-05-09 22:30:38 -0600

    testing session factory

--- src/main/java/com/ramsey/contactmgr/Application.java
+++ src/main/java/com/ramsey/contactmgr/Application.java
@@ -2,6 +2,7 @@ package com.ramsey.contactmgr;
 
 
 import com.ramsey.contactmgr.model.Contact;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -12,7 +13,7 @@ import javax.imageio.spi.ServiceRegistry;
 public class Application {
 
     //Reusable ref to a session factory
-    //private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         //Creating a standard service registry object
@@ -25,6 +26,19 @@ public class Application {
                 .withEmail("joel@gmail.com")
                 .withPhone(5555555555L)
                 .build();
-        System.out.println(contact);
+        // Open session
+        Session session = sessionFactory.openSession();
+
+        //Begin transaction
+        session.beginTransaction();
+
+        //Use the session to save the transaction
+        session.save(contact);
+
+        //Commit the transaction
+        session.getTransaction().commit();
+
+        //Close the session
+        session.close();
     }
 }
