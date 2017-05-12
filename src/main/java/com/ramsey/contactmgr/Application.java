b6f7e908e2a82494ada7ef8bf75b0e8396fba9b6
joelramsey <joel.d.ramsey@gmail.com>
2017-05-11 20:49:23 -0600

    added more crud to this thing

--- src/main/java/com/ramsey/contactmgr/Application.java
+++ src/main/java/com/ramsey/contactmgr/Application.java
@@ -2,6 +2,7 @@ package com.ramsey.contactmgr;
 
 
 import com.ramsey.contactmgr.model.Contact;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -9,6 +10,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import javax.imageio.spi.ServiceRegistry;
+import java.util.List;
 
 public class Application {
 
@@ -26,6 +28,44 @@ public class Application {
                 .withEmail("joel@gmail.com")
                 .withPhone(5555555555L)
                 .build();
+
+        int id = save(contact);
+
+        // Display a list of contacts before update
+        System.out.printf("%n%nBefore update%n%n");
+        fetchAllContacts().stream().forEach((System.out::print));
+
+        // Get persisted contact
+        Contact c = findContactById(id);
+
+        // Update the contact
+        c.setFirstName("Myself");
+
+        // Persist the changes
+        System.out.printf("%n%nUpdating...%n%n");
+        update(c);
+        System.out.printf("%n%nUpdate complete!%n%n");
+
+        // Display a list of contacts after the update
+        System.out.printf("%n%nAfter%n%n");
+        fetchAllContacts().stream().forEach((System.out::print));
+    }
+
+    private static  Contact findContactById(int id) {
+        // Open session
+        Session session = sessionFactory.openSession();
+
+        //Create a criteria object
+        Contact contact = session.get(Contact.class,id);
+
+        //Close the session
+        session.close();
+
+        //Return the contact  object
+        return contact;
+    }
+
+    private static void update(Contact contact) {
         // Open session
         Session session = sessionFactory.openSession();
 
@@ -33,12 +73,48 @@ public class Application {
         session.beginTransaction();
 
         //Use the session to save the transaction
-        session.save(contact);
+        session.update(contact);
 
         //Commit the transaction
         session.getTransaction().commit();
 
         //Close the session
         session.close();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts() {
+        // Open session
+        Session session = sessionFactory.openSession();
+
+        //Create a criteria object
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        //Get a list of contact objects according to the Criteria object
+        List<Contact> contacts = criteria.list();
+
+        //Close the session
+        session.close();
+
+        return contacts;
+    }
+
+    private static int save(Contact contact) {
+        // Open session
+        Session session = sessionFactory.openSession();
+
+        //Begin transaction
+        session.beginTransaction();
+
+        //Use the session to save the transaction
+        int id = (int)session.save(contact);
+
+        //Commit the transaction
+        session.getTransaction().commit();
+
+        //Close the session
+        session.close();
+
+        return id;
     }
 }
